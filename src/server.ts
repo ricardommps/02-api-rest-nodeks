d776d1800b24eadf1caf5eed9c1dd9b4321da614
Ricardo Matta <ricardommps@gmail.com>
2024-05-07 11:40:30 -0300

    chore: Build config host

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,11 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => console.log('Http server running'))
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP server Running')
+  })
